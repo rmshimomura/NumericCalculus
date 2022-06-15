fa1b150461577ff146ef93344d11deb8d8e0de6b
rmshimomura <shimomurarodrigo1@gmail.com>
2022-06-15 00:45:30 -0300

    Updated general.py removing the real value

--- simple_integration/general.py
+++ simple_integration/general.py
@@ -15,8 +15,6 @@ trapezoid_result = round(trapezoid(xi, xf, n, function), 6)
 simpson1_3_result = round(simpson1_3(xi, xf, n, function), 6)
 simpson3_8_result = round(simpson3_8(xi, xf, n, function), 6)
 
-real_result = -3.257739
-
-print(f"\nTrapezoid = {trapezoid_result} error = {round(abs(trapezoid_result - real_result), 6)}")
-print(f"Simpson 1/3 = {simpson1_3_result} error = {round(abs(simpson1_3_result - real_result), 6)}")
-print(f"Simpson 3/8 = {simpson3_8_result} error = {round(abs(simpson3_8_result - real_result), 6)}\n")
+print(f"\nTrapezoid = {trapezoid_result}")
+print(f"Simpson 1/3 = {simpson1_3_result}")
+print(f"Simpson 3/8 = {simpson3_8_result}\n")
